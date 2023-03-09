5a89c98ff20eb3d1aab667599317c8974c00df65
xingyi.hu <xingyi.hu@shopee.com>
2023-03-09 09:11:34 +0800

    feat: add prisma

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const Home: NextPage = () => {
+  return <div>Hello Home Page</div>;
+};
+
+export default Home;
